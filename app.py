cebb152bcfe8f18813b86d6136eb49bebc0e22d1
Noah Syrkis <noah@syrkis.com>
2023-07-24 23:11:32 +0200

    update steamlit

--- app.py
+++ app.py
@@ -7,15 +7,31 @@ import os
 
 @st.cache_data
 def load_data():
-    fps = ['power_density_mask', 'wind_speed_mask', 'M']
-    names = ['Power Density', 'Wind Speed', 'All']
-    data = {name: np.load(os.path.join("masks", fp + '.npy')) for fp, name in zip(fps, names)}
+    fps = ['power_density', 'powerlines', 'population_mask', 'wind_speed', 'protected_areas_mask', 'airport_mask', 'slope_mask', 'roughness', 'brazil_mask']
+    names = ['Power Density', 'Power Lines', 'Population', 'Wind Speed', 'Protected Areas', 'Airports', 'Slope', 'Roughness', 'Brazil']
+    data = {name: np.load(os.path.join("masks", fp + '.npy')).astype(float) for fp, name in zip(fps, names)}
+    data['Power Density'] /= (data['Power Density'].max() / 100)
+    data['Population'] /= (data['Population'].max() / 100)
+    data['Roughness'] = data['Roughness'] * data['Brazil']
+    data['Roughness'] /= (data['Roughness'].max() / 100)
     return data
 
-def weighted_sum(data, weights):
-    mask = sum([data[name] * weight for name, weight in weights.items()])
-    mask = np.where(mask > 0.5, 1, 0)
+def make_mask(data, thresholds):
+    mask = np.ones_like(data['Power Density'])
+    for key, threshold in thresholds.items():
+        # if key in ['']
+        mask *= data[key] > threshold
     return mask
+
+def make_mask(frame, theshold=None, over=True):
+    if theshold and over:
+        frame = frame > theshold
+    elif theshold and not over:
+        frame = frame < theshold
+    frame = frame.astype(float)
+    frame /= frame.max()
+    return frame
+
 
 
 data = load_data()
@@ -35,30 +51,57 @@ st.sidebar.title("Explorer")
 region = st.sidebar.selectbox("Region", ['All', 'Northeast', 'South', 'Southeast', 'North', 'Center-West'])
 
 
-power_density = st.sidebar.slider("Power Density", 0, 10, 5) / 10
-wind_speed = st.sidebar.slider("Wind Speed", 0, 10, 5) / 10
-biodiversity = st.sidebar.slider("Biodiversity", 0, 10, 5) / 10
-roughness = st.sidebar.slider("Roughness", 0, 10, 5) / 10
-
+st.sidebar.markdown("Select factor thresholds")
+power_density = st.sidebar.slider("Power density (percentile)", 0, 100, 50) + 1
+grid_distance = 31  - st.sidebar.slider("Grid distance (km)", 0, 30, 10)
+# population = 101 - st.sidebar.slider("Population count", 0, 100, 50)
+roughness = st.sidebar.slider("Rougness (percentile)", 0, 100, 50) + 1
+wind_speed = st.sidebar.slider("Minimum wind speed (m/s)", 5, 10, 7) - 1
 
 # Create three columns for the images
+power_density_mask = make_mask(data['Power Density'], power_density)
+grid_distance_mask = make_mask(data['Power Lines'], grid_distance)
+# population_mask = make_mask(data['Population'], population)
+roughness_mask = make_mask(data['Roughness'], roughness)
+wind_speed_mask = make_mask(data['Wind Speed'], wind_speed)
+
+final_mask = (power_density_mask * grid_distance_mask * roughness_mask * wind_speed_mask).astype(float)
 
 
 # Visualization
-mask = weighted_sum(data, {'Power Density': power_density, 'Wind Speed': wind_speed})
-st.image(mask, caption='Final mask', use_column_width=True)
+# mask = make_mask(data, {'Power Density': power_density, 'Wind Speed': wind_speed})
+st.image(final_mask, caption='Final mask', use_column_width=True)
 
 st.write(f"""The map above shows the areas that are suitable for wind power generation.
          It is a weighted sum of the maps below, where you can see the individual factors that were considered.""")
 
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col4, col5 = st.columns(4)
+
 # Display images in the three columns
-col1.image(data['All'], caption='Power Density', use_column_width=True)
-col2.image(data['All'], caption='Wind Speed', use_column_width=True)
-col3.image(data['All'], caption='All', use_column_width=True)
-col4.image(data['All'], caption='All', use_column_width=True)
+col1.image(power_density_mask, caption='Power Density', use_column_width=True)
+col2.image(grid_distance_mask, caption='Power Lines', use_column_width=True)
+# col3.image(population_mask, caption='Population', use_column_width=True)
+col4.image(roughness_mask, caption='Roughness', use_column_width=True)
+col5.image(wind_speed_mask, caption='Wind Speed', use_column_width=True)
 
 
 # about the project
-st.title("Behind the scenes")
+st.title("More info on the map construction")
+st.write(f"""In addition to the modalities that you control,
+        the map also excludes areas that are not suitable for wind power generation,
+        biodiversity conservation areas and areas with high roughness, and population.
+         The constraints and the base topography can be seen below.""")
+
+# new columns
+col1, col2, col3 = st.columns(3)
+
+
+col1.image(data['Airports'], caption='Airports', use_column_width=True)
+col2.image(data['Protected Areas'], caption='Protected Areas', use_column_width=True)
+col3.image(data['Slope'], caption='Slope', use_column_width=True)
+
+
+# footer
+st.write(f"""For more information on the potential of wind power generation in Brazil,
+        contact the [Brazilian Climate Center.](https://doe.climatempo.com.br/cbc)""")
